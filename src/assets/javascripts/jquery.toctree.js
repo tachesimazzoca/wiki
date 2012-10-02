f5082f8c8cea665dd455178da33bb298647aa62c
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2012-10-03 02:45:03 +0900

    Replace the "toctree" script

--- src/assets/javascripts/jquery.toctree.js
+++ src/assets/javascripts/jquery.toctree.js
@@ -1,15 +1,16 @@
 (function($) {
 
-    $.fn.toc = function(option) {
+    $.fn.toctree = function(option) {
 
-        options = option || {};
+        option = option || {};
+        option.selector = option.selector || ':header';
         option.offset = parseInt(option.offset) || 0;
         option.depth = parseInt(option.depth) || 3;
-        option.selector = option.selector || ':header';
+        option.prefix = option.prefix || 'toctree-';
 
-        var toc = this;
+        var $toc = this;
 
-        var ul = toc.append('<ul></ul>').children('ul');
+        var ul = $toc.append('<ul></ul>').children('ul');
 
         var last = 1;
         var section = '';
@@ -21,11 +22,10 @@
                 if (num == NaN || num <= option.offset || option.depth < num - option.offset) {
                     return;
                 }
-                var header = $(this);
+                var $header = $(this);
                 if (last != num) {
-                    ul = toc.children('ul');
+                    ul = $toc.children('ul');
                     section = '';
-                    counter = 0;
                     for (var n = 1; n < (num - option.offset); n++) {
                         if (ul.find('> li:last').length === 0) {
                             ul.append('<li><ul></ul></li>');
@@ -33,14 +33,15 @@
                         if (ul.find('> li:last > ul').length === 0) {
                             ul.find('> li:last').append('<ul></ul>');
                         }
-                        section += ul.find('> li').length + '_';
+                        section += ul.find('> li').length + '-';
                         ul = ul.find('> li:last').children('ul');
                     }
+                    counter = ul.find('> li').length;
                 }
                 counter++;
-                anchor = 'section_' + section + counter;
-                ul.append('<li><a href="#' + anchor + '">' + $(this).text() + '</a></li>');
-                header.before('<a id="' + anchor + '"></a>');
+                anchor = option.prefix + section + counter;
+                ul.append('<li><a href="#' + anchor + '">' + $header.text() + '</a></li>');
+                $header.before('<a id="' + anchor + '"></a>');
                 last = num;
             });
         });
