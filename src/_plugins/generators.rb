5d046b14fdc9ad2bf71e2bd974c3ad4065bed388
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2012-10-04 18:03:31 +0900

    Add SitemapGenerator

---
+++ src/_plugins/generators.rb
@@ -0,0 +1,50 @@
+require 'fileutils'
+require 'time'
+
+module Jekyll
+
+  class SitemapFile < StaticFile
+    def write(dest)
+      false
+    end
+  end
+
+  class SitemapGenerator < Generator
+    safe true
+    priority :low
+
+    def initialize(config = {})
+      super
+      @outfile = 'sitemap.xml'
+    end
+
+    def generate(site)
+
+      out = File.join(site.dest, @outfile)
+
+      FileUtils.mkdir_p(File.dirname(out))
+
+      File.open(out, 'w') do |f|
+
+        f.write('<?xml version="1.0" encoding="UTF-8"?>')
+        f.write("\n" + '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">')
+
+        baseloc = "#{site.config['url']}".gsub(/\/+$/, "")
+        baseloc << "#{site.config['baseurl']}".gsub(/\/+$/, "")
+
+        xml = ""
+
+        site.pages.each do |page|
+          path = File.dirname(page.destination("")).gsub(/\/+$/, "") + "/" + page.name
+          lastmod = FileTest.exist?(site.source + path) ? File.mtime(site.source + path) : Time.now
+          xml << "\n<url><loc>#{baseloc}#{page.destination("")}</loc><lastmod>#{lastmod.strftime("%Y-%m-%dT%H:%M:%S%z")}</lastmod></url>"
+        end
+
+        f.write(xml)
+        f.write("\n" + '</urlset>')
+      end
+
+      site.static_files << SitemapFile.new(site, site.dest, '/', @outfile) if File.exists?(out)
+    end
+  end
+end
