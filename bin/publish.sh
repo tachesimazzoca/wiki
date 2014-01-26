d48036d4e42c76e32d4f79e3c6e74dbc947321e4
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2014-01-26 16:08:43 +0900

    Use ssh scheme for publishing

--- bin/publish.sh
+++ bin/publish.sh
@@ -14,7 +14,7 @@ out=${tmpdir}com.github.tachesimazzoca.wiki.$$
 
 trap "rm -fr $out" 0 1 2
 
-git clone "git://github.com/tachesimazzoca/tachesimazzoca.github.com.git" $out &&
+git clone "ssh://github-tachesimazzoca/tachesimazzoca/tachesimazzoca.github.com.git" $out &&
 rsync -avz --delete src/_site/ $out/wiki/ &&
 
 cd $out &&
