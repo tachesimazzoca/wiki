025747ba578b3e59bc5cb406e96233c9ed05874d
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2014-01-26 15:54:45 +0900

    Improve publish script

--- bin/publish.sh
+++ bin/publish.sh
@@ -4,22 +4,28 @@ cd `dirname ${0}`/..
 
 message="Publish tachesimazzoca/wiki@"`git log --oneline | head -n 1 | awk '{print $1}'`
 
-out=/tmp/tachesimazzoca.wiki.$$
+tmpdir=$TMPDIR
+if [ -z "$tmpdir" ];
+then
+  tmpdir=/tmp/
+fi
+
+out=${tmpdir}com.github.tachesimazzoca.wiki.$$
 
 trap "rm -fr $out" 0 1 2
 
-git clone "git@github.com:tachesimazzoca/tachesimazzoca.github.com.git" $out
-rsync -avz --delete src/_site/ $out/wiki/
+git clone "git://github.com/tachesimazzoca/tachesimazzoca.github.com.git" $out &&
+rsync -avz --delete src/_site/ $out/wiki/ &&
 
-cd $out
-git config user.name "Takeshi Matsuoka"
-git config user.email "tachesimazzoca@gmail.com"
-git add wiki 
+cd $out &&
+git config user.name "Takeshi Matsuoka" &&
+git config user.email "tachesimazzoca@gmail.com" &&
+git add wiki &&
 
 git commit -a -e -m "${message}"
 
 echo -n "Are you sure?"
-read INPUT 
+read INPUT
 
 case "$INPUT" in
     "y") git push origin master ;;
