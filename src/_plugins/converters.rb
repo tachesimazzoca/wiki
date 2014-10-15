5ee5b66e578fe1d624482649270b6f129d99c7b9
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2014-10-15 16:47:06 +0900

    Patch Pygments::Popen#alive for re-build performance

--- src/_plugins/converters.rb
+++ src/_plugins/converters.rb
@@ -1,2 +1,11 @@
-module Jekyll
+require 'pygments'
+
+module Pygments
+  module Popen
+    # Skip Process.kill(0, @pid) for the re-generation performance.
+    # It might cause any exception, but we rely on the PID.
+    def alive?
+      !!@pid
+    end
+  end
 end
