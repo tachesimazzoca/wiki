2558e09b8d9bf6868afaff609de48ab0edfd9143
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2015-01-22 19:14:48 +0900

    Add .noindex feature to SitemapGenerator
    - layout/home no longer uses the toctree tag

--- src/_plugins/generators.rb
+++ src/_plugins/generators.rb
@@ -16,6 +16,18 @@ module Jekyll
     def initialize(config = {})
       super
       @outfile = 'sitemap.xml'
+
+      # Load the patterns of the ignored files via /.noindex
+      noindex_f = "#{config['source']}/.noindex"
+      if File.exist?(noindex_f)
+        @noindex = (File.open(noindex_f).readlines.map! do |v|
+          v.strip
+        end).select do |v|
+          !v.empty?
+        end
+      else
+        @noindex = []
+      end
     end
 
     def generate(site)
@@ -36,8 +48,14 @@ module Jekyll
 
         site.pages.each do |page|
           next unless page.destination("").match(/\.html$/)
+
+          next if @noindex.any? do |ptn|
+            escaped = Regexp.escape(ptn).gsub('\*','.*?')
+            Regexp.new("^#{escaped}$") =~ "/#{page.path}"
+          end
+
           path = File.dirname(page.destination("")).gsub(/\/+$/, "") + "/" + page.name
-          lastmod = (FileTest.exist?(site.source + path) ? File.mtime(site.source + path) : Time.now)
+          lastmod = (File.exist?(site.source + path) ? File.mtime(site.source + path) : Time.now)
             .strftime("%Y-%m-%dT%H:%M:%S%z").gsub(/00$/, ':00')
           xml << "\n<url><loc>#{baseloc}#{page.destination("")}</loc><lastmod>#{lastmod}</lastmod></url>"
         end
--- src/index.md
+++ src/index.md
@@ -3,3 +3,28 @@ layout: home
 
 title: Home
 ---
+
+## Index of Pages
+
+* [Android](android/index.html)
+* [Amazon Web Services](aws/index.html)
+* [English](english/index.html)
+* [Git](git/index.html)
+* [gitolite](gitolite/index.html)
+* [Hadoop](hadoop/index.html)
+* [Haskell](haskell/index.html)
+* [httpd](httpd/index.html)
+* [Java](java/index.html)
+* [Linux](linux/index.html)
+* [Mahout](mahout/index.html)
+* [Math](math/index.html)
+* [MySQL](mysql/index.html)
+* [PostgreSQL](pgsql/index.html)
+* [Postfix](postfix/index.html)
+* [Rails3](rails3/index.html)
+* [Ruby Version Manager](rvm/index.html)
+* [sbt](sbt/index.html)
+* [Scala](scala/index.html)
+* [Vagrant](vagrant/index.html)
+* [VirtualBox](virtualbox/index.html)
+
