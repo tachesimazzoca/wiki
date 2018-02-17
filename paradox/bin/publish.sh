7bd76f6561f1cb5a3a781300ce53829687088bc5
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2018-02-17 19:34:22 +0900

    Add scripts

---
+++ paradox/bin/publish.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+cd `dirname ${0}`/..
+
+message="Publish tachesimazzoca/wiki@"`git log --oneline | head -n 1 | awk '{print $1}'`
+
+tmpdir=$TMPDIR
+if [ -z "$tmpdir" ];
+then
+  tmpdir=/tmp/
+fi
+
+out=${tmpdir}com.github.tachesimazzoca.wiki.$$
+
+trap "rm -fr $out" 0 1 2
+
+git clone "ssh://github-tachesimazzoca/tachesimazzoca/tachesimazzoca.github.com.git" $out &&
+rsync -avz --delete target/paradox/site/main/ $out/wiki/ &&
+
+cd $out &&
+git config user.name "Takeshi Matsuoka" &&
+git config user.email "tachesimazzoca@gmail.com" &&
+git add wiki &&
+
+git commit -a -e -m "${message}"
+
+echo -n "Are you sure?"
+read INPUT
+
+case "$INPUT" in
+    "y") git push origin master ;;
+    *) break ;;
+esac
+
+exit 0
